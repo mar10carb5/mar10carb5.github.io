be8c6c4a9289b1d317eb144a4ac7ecc61ca41138
mar10carb5 <mdc8719@outlook.com>
2024-12-08 19:41:10 -0500

    Add files via upload

---
+++ Project 1 FINAL/styles2.css
@@ -0,0 +1,30 @@
+
+header {
+    background: #d62828; 
+    color: #ffffff;
+    padding: 45px 0;
+    text-align: center;
+	font-size: 27px;
+}
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #CFB53B;
+  color: white;
